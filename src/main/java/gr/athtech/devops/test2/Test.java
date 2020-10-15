8dc5f47202fd1ac723c890ac07603598561a0617
ThomasVarsamidis <ThomasVarsamidis>
2020-10-15 17:15:22 +0300

    Initial commit

---
+++ src/main/java/gr/athtech/devops/test2/Test.java
@@ -0,0 +1,18 @@
+package gr.athtech.devops.test2;
+
+public class Test {
+
+    private static String getMessage() {
+        return "A message";
+    }
+
+    private static String getHello() {
+        return "Hello 2";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(getMessage());
+    }
+
+}
